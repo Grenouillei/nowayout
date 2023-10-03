18ca0f12769bf24ca16706912d2f4ae70c4cd3c6
Yuliy Futismky <yulikfwork@gmail.com>
2023-10-03 15:28:45 +0300

    few

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -40,16 +40,6 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'password' => 'hashed',
     ];
-
-    public function roles() {
-        return $this->belongsToMany(Role::class,'user_roles');
-    }
-
-    public function hasRole($alias){
-        foreach ($this->roles as $role)
-            if ($role->alias == $alias)
-                return true;
-        return false;
-    }
 }
